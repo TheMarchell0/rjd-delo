1f9dc60dd939ac655853ed2fac1844c24efa7380
TheMarchell0 <TheMarchell0@yandex.ru>
2024-08-12 06:01:20 +0600

    add attributes blocks

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const mainBlock = document.querySelector('.game'),
     typeButton = mainBlock.querySelector('.js-type-button'),
     nextButtons = mainBlock.querySelectorAll('.js-next-button'),
     homeButtons = mainBlock.querySelectorAll('.js-home-icon'),
-    typeBlocks = mainBlock.querySelectorAll('.js-type');
+    typeBlocks = mainBlock.querySelectorAll('.js-type'),
+    chooseLists = mainBlock.querySelectorAll('.js-choose-list');
 
 let currentBlock = 'main',
     nextBlock = '';
@@ -10,6 +11,27 @@ let currentBlock = 'main',
 window.addEventListener('DOMContentLoaded', function () {
 
     gsap.to(".main", {opacity: 1, duration: 1, zIndex: 10})
+
+    for (let chooseList of chooseLists) {
+        const chooseItems = chooseList.querySelectorAll('.js-choose-item');
+        for (let chooseItem of chooseItems) {
+            chooseItem.addEventListener('click', () => {
+                const radioInput = chooseItem.querySelector('input[type="radio"]');
+                if (radioInput) {
+                    const activeItem = chooseList.querySelector('.js-choose-item.active');
+                    if (activeItem && activeItem !== chooseItem) {
+                        activeItem.classList.remove('active');
+                    }
+
+                    if (!chooseItem.classList.contains('active')) {
+                        chooseItem.classList.add('active');
+                    }
+                } else {
+                    chooseItem.classList.toggle('active');
+                }
+            })
+        }
+    }
 
     for (let typeBlock of typeBlocks) {
         typeBlock.addEventListener('click', () => {
@@ -59,6 +81,12 @@ window.addEventListener('DOMContentLoaded', function () {
             currentBlock = 'main',
                 nextBlock = '';
             gsap.to('.game-flex__footer-button', {disabled: true})
+            const activeItems = mainBlock.querySelectorAll('.js-choose-item.active')
+            if (activeItems) {
+                for (let activeItem of activeItems) {
+                    activeItem.classList.remove('active')
+                }
+            }
         })
     }
 });
